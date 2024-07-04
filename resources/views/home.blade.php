8ec5e08cf7b59977b68e7c81594de9eb032f3e28
daffa09 <daffa.fathan9@gmail.com>
2024-07-04 21:03:38 +0700

    Refactor HomeController to remove unused Customer model and related code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,18 +49,7 @@
           </div>
           <!-- ./col -->
           <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-              <div class="inner">
-                <h3>{{$customers_count}}</h3>
 
-                <p>{{ __('dashboard.Customers_Count') }}</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-person-add"></i>
-              </div>
-              <a href="{{ route('customers.index') }}" class="small-box-footer">{{ __('common.More_info') }} <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
           </div>
           <!-- ./col -->
     </div>
